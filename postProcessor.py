dd2b731bd03fe234bef2a21de4499b77c8246401
ryanhumps <inbox@hremail.net>
2023-10-29 22:11:51 -0700

    postProcessor.py: refactor

--- postProcessor.py
+++ postProcessor.py
@@ -4,39 +4,51 @@ import re
 import os
 import fnmatch
 
+# declarations
 sourceFile=sys.argv[1]
-myFloat = 0.0
+eLen = 0.0
+slackInterval = 250.0
 
-# Read the ENTIRE g-code file into memory
+# read file into memory
 with open(sourceFile, "r") as f:
     lines = f.readlines()
 f.close()
 
+# "write-in-place" the result
 destFile = sourceFile
 os.remove(sourceFile)
 
+
 with open(destFile, "w") as of:
-    for lIndex in range(len(lines)):
-        oline = lines[lIndex]
-        # Parse gcode line
-        parts = oline.split(';', 1)
+    for i in range(len(lines)):
+        line = lines[i]
+
+        # get non-comment portion of line
+        parts = line.split(';', 1)
         if len(parts) > 0:
-            
-		    # Parse command
+
             command = parts[0].strip()
             
-			# Write original line       
-            of.write(oline)
+			# write original line       
+            of.write(line)
 
+            # check command starts with G1
             if command:
-                stringMatch = re.search ('^G1', command)
-                if stringMatch:
-                    splice = command.split(' ', -1)
-                    eMatch = fnmatch.filter(splice, 'E*')
-                    if len(eMatch) > 0:
-                        myFloat += float(eMatch[0].replace('E',''))
-                        if myFloat>250:
-                            myFloat -= 250
-                            of.write('Ts\n')
+                g1Match = re.search ('^G1', command)
+
+                # check G1 command has extrusion
+                if g1Match:
+                    axes = command.split(' ', -1)
+                    eAxis = fnmatch.filter(axes, 'E*')
+                    if len(eAxis) > 0:
+
+                        #add extrusion length to running total
+                        eLen += float(eAxis[0].replace('E',''))
+
+                        # if running total exceeds slack interval
+                        # write a Ts command and reduce the running total
+                        if eLen>slackInterval:
+                            eLen -= slackInterval
+                            of.write('Ts ; Slack {} mm\n'.format(slackInterval))
                             
 of.close()
